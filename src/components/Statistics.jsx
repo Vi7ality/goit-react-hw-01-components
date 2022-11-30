65773ecdbf7dc1d31bc14f498f46b910cc6885ae
Vitalii <svitalii138@gmail.com>
2022-11-30 21:41:30 +0200

    №1, 2 added

---
+++ src/components/Statistics.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+
+export const Statistics = ( { stats }) => (
+    <section className="statistics">
+        {title (<h2 className="title">{ title }</h2>)}
+
+
+  <ul className="stat-list">
+            {stats.map(stat => (
+        <li className="item" key={stat.id}>
+      <span className="label">{stat.label}</span>
+      <span className="percentage">{stat.percentage}</span>
+    </li>
+))}
+
+  </ul>
+</section>
+)
+
+Statistics.propTypes = {
+    stat: PropTypes.exact(
+{        id: PropTypes.string.isRequired,
+            label: PropTypes.string.isRequiered,
+        percentage: PropTypes.number.isRequired,}
+
+    )
+}
